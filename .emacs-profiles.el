278f3eef9fa8a86ac814ab3db5a0492d10f66379
Paul Meier <paulmartinmeier@gmail.com>
2022-07-16 15:32:46 -0500

    changes from kirby on Sat Jul 16 03:32:46 PM CDT 2022

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,3 @@
 (("doom"      . ((user-emacs-directory   . "~/.emacs-configs/emacs-doom")))
  ("pmacs"     . ((user-emacs-directory   . "~/.emacs-configs/pmacs")))
- ("efs"       . ((user-emacs-directory   . "~/.emacs-configs/efs"))))
+ ("crafted"   . ((user-emacs-directory   . "~/.emacs-configs/crafted-emacs"))))
