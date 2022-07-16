a86e3e1430dad605c6bc295cc7c734ee121a33ca
Paul Meier <paulmeier@northwesternmutual.com>
2022-07-16 15:24:25 -0500

    adding org config files for doom cust

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -104,14 +104,14 @@
 
 (defun efs/org-babel-tangle-config ()
   (when (string-equal (buffer-file-name)
-                      (expand-file-name "~/.dotfiles/Zsh.org"))
+                      (expand-file-name "Zsh.org"))
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
 
 (defun efs/org-babel-tangle-config ()
   (when (string-equal (buffer-file-name)
-                      (expand-file-name "~/.dotfiles/Doom.org"))
+                      (expand-file-name "doom.config.el.org"))
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
@@ -167,39 +167,3 @@
   (setq org-babel-default-header-args:jupyter-python '((:async . "yes")
                                                        (:session . "py")
                                                        (:kernel . "python3"))))
-
-(defun copy-to-clipboard ()
-  "Copies selection to x-clipboard"
-  (interactive)
-  (if (display-graphic-p)
-      (progn
-        (message "Yanked region to x-clipboard")
-        (call-interactively 'clipboard-kill-ring-save)
-        )
-    (if (region-active-p)
-        (progn
-          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
-          (message "Yanked region to clipboard")
-          (deactivate-mark))
-      (message "No region active; can't yank to clipboard!")))
-  )
-
-(defun paste-from-clipboard ()
-  "Pastes from x-clipboard"
-  (interactive)
-  (if (display-graphic-p)
-      (progn
-        (clipboard-yank)
-        (message "graphics active")
-        )
-    (insert (shell-command-to-string "xsel -o -b"))
-    )
-  )
-
-(map! :leader
-      :desc "copy-to-clipboard"
-      "o y" #'copy-to-clipboard)
-
-(map! :leader
-      :desc "paste-from-clipboard"
-      "o p" #'paste-from-clipboard)
