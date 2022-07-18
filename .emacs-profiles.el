8e9e4ad7f04139c2d88424440958be786bd5d6f4
Paul Meier <paulmeier@northwesternmutual.com>
2022-07-18 14:53:20 -0500

    changes from s292266.nml.com on Mon Jul 18 14:53:19 CDT 2022

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,3 @@
 (("doom"      . ((user-emacs-directory   . "~/.emacs-configs/emacs-doom")))
- ("crafted"   . ((user-emacs-directory   . "~/.emacs-configs/crafted-emacs"))))
+ ("crafted"   . ((user-emacs-directory   . "~/.emacs-configs/crafted-emacs")
+                 (env . (("CRAFTED_EMACS_HOME" . "~/.config/crafted-config"))))))
