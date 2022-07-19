100529ead9a65b815d79043b8619836c3c671cda
Paul Meier <paulmartinmeier@gmail.com>
2022-07-18 21:18:50 -0500

    crafted-config rename

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,2 @@
 (("doom"      . ((user-emacs-directory   . "~/.emacs-configs/emacs-doom")))
- ("crafted"   . ((user-emacs-directory   . "~/.emacs-configs/crafted-emacs")
-                 (env . (("CRAFTED_EMACS_HOME" . "~/.config/crafted-config"))))))
+ ("crafted"   . ((user-emacs-directory   . "~/.emacs-configs/crafted-emacs"))))
