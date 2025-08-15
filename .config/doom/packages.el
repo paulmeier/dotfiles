c43c6f0abaf80d8281348ab064de3ce85d2ae76c
Paul Meier <paulmeier@northwesternmutual.com>
2025-08-15 10:08:08 -0500

    init work config

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,28 +1,60 @@
 ;; -*- no-byte-compile: t; -*-
-;;; ~/.config/doom/packages.el
+;;; $DOOMDIR/packages.el
 
-;;; Examples:
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+
+
+;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! some-package)
-;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+
+;; To install a package directly from a remote git repo, you must specify a
+;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; https://github.com/radian-software/straight.el#the-recipe-format
+;; (package! another-package
+;;   :recipe (:host github :repo "username/repo"))
+
+;; If the package you are trying to install does not contain a PACKAGENAME.el
+;; file, or is located in a subdirectory of the repo, you'll need to specify
+;; `:files' in the `:recipe':
+;; (package! this-package
+;;   :recipe (:host github :repo "username/repo"
+;;            :files ("some-file.el" "src/lisp/*.el")))
+
+;; If you'd like to disable a package included with Doom, you can do so here
+;; with the `:disable' property:
 ;; (package! builtin-package :disable t)
 
-;; AI
-;;(package! gptel)
+;; You can override the recipe of a built in package without having to specify
+;; all the properties for `:recipe'. These will inherit the rest of its recipe
+;; from Doom or MELPA/ELPA/Emacsmirror:
+;; (package! builtin-package :recipe (:nonrecursive t))
+;; (package! builtin-package-2 :recipe (:repo "myfork/package"))
+
+;; Specify a `:branch' to install a package from a particular branch or tag.
+;; This is required for some packages whose default branch isn't 'master' (which
+;; our package manager can't deal with; see radian-software/straight.el#279)
+;; (package! builtin-package :recipe (:branch "develop"))
+
+;; Use `:pin' to specify a particular commit to install.
+;; (package! builtin-package :pin "1a2b3c4d5e")
+
+
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+;; (unpin! pinned-package)
+;; ...or multiple packages
+;; (unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+;; (unpin! t)
+
+;; Drag Lines
+(package! drag-stuff)
 
 ;; Org
-;;(package! org-super-agenda)
-(package! ob-mermaid)
-;;(package! org-view-mode)
-
-;; Org Roam
-;;(package! org-roam-ui
-;;   :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
-;; (package! citar-org-roam)
-
-;; Emacs
 (package! valign)
 (package! centered-cursor-mode)
-(package! good-scroll)
+(package! ob-mermaid)
 
 ;; HTTP
 (package! verb)
@@ -35,35 +67,22 @@
 (package! denote-menu)
 (package! consult-denote)
 (package! consult-notes)
+(package! obsidian)
 
 ;; Programming Stuff
 (package! load-env-vars)
-
-;; Drag Lines
-(package! drag-stuff)
 
 ;; AI
 (package! copilot-chat
   :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el")))
 (package! copilot)
-;;(package! gptel :recipe (:nonrecursive t))
 
-;; Kubernetes
-;;(package! kubernetes)
-;;(package! kubernetes-evil)
-;;(package! kubedoc)
-;;(package! k8s-mode)
+;; Snippets
+(package! doom-snippets)
 
 ;; Loaded packages for work
 (when (string= (system-name) "S427544")
   (package! lab) ;; Gitlab
   (package! impostman) ;; Postman importing
   (package! jinja2-mode) ;; jinja2 support
-  ;;
   )
-
-;; Obsidian
-(package! obsidian)
-
-;; Jupyter
-;; (package! jupyter)
